f48c175a1d7ebc5b0bfae068854bd499be61a9ec
Arnaud875 <arnaud.nickles33@gmail.com>
2024-06-18 10:20:12 +0000

    script

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - y
+ * Return: yes
+ */
+int main(void)
+{
+char a;
+
+for (a = 'a'; a <= 'z'; a++)
+{
+if(a == 'q' || a == 'e')
+{
+continue;
+}
+putchar(a);
+}
+
+putchar('\n');
+
+return (0);
+}
