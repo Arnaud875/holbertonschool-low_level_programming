c44bd621e868e7b8486a96e97860e5a1eacf4815
Arnaud875 <arnaud.nickles33@gmail.com>
2024-06-18 10:22:17 +0000

    script

---
+++ variables_if_else_while/5-print_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - y
+ * Return: yes
+ */
+int main(void)
+{
+int a;
+
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+
+putchar('\n');
+
+return (0);
+}
