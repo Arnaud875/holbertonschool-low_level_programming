3564f7f634ed645ded76d6c3f5e39d9bdf8d24a1
Arnaud875 <arnaud.nickles33@gmail.com>
2024-06-18 10:25:34 +0000

    script

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - y
+ * Return: yes
+ */
+int main(void)
+{
+char a;
+
+for (a = 'z'; a >= 'a'; a--)
+{
+putchar(a);
+}
+
+putchar('\n');
+
+return (0);
+}
