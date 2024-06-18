e604e383c33a37fcdad436f9554c0827dff56dff
Arnaud875 <arnaud.nickles33@gmail.com>
2024-06-18 14:29:22 +0000

    script

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 if (n == 0)
 {
-write(1, '0', 1);
+write(1, "0", 1);
 return (0);
 }
 else if (n > 0)
 {
-write(1, '+', 1);
+write(1, "+", 1);
 return (1);
 }
 else
 {
-write(1, '-', 1);
+write(1, "-", 1);
 return (-1);
 }
 }
