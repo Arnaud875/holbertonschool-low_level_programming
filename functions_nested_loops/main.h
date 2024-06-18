971796be9172cf196c0ada8a737553421f709f4c
Arnaud875 <arnaud.nickles33@gmail.com>
2024-06-18 14:35:14 +0000

    script

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,15 @@
+#ifndef _HELLO
+#define _HELLO
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * _putchar - yes
+ * @c: yes
+ * Return: yes
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
@@ -12,3 +20,4 @@ void print_alphabet_x10(void);
 int _islower(int);
 int _isalpha(int);
 int print_sign(int);
+#endif
