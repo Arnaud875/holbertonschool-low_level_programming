a8aa0ad15c4b71f51c6bdc5086a72622afcb5adb
Arnaud875 <arnaud.nickles33@gmail.com>
2024-06-18 10:53:36 +0000

    script

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - y
+ * Return: y
+ */
+void print_alphabet_x10(void)
+{
+char a;
+int b;
+
+for (b = 0; b < 10; b++)
+{
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+
+_putchar('\n');
+}
+
+_putchar('\n');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 void print_alphabet(void);
 void _putchar(int a);
+void print_alphabet_x10(void);
