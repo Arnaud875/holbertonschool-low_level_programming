b665db49af0dcd3b316722f0a19022f8b9419983
Arnaud875 <arnaud.nickles33@gmail.com>
2024-06-18 10:11:18 +0000

    script

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,15 @@
  */
 int main(void)
 {
-const char *a = "abcdefghijklmnopqrstuvwxyz\n";
+const char *a = "abcdefghijklmnopqrstuvwxyz";
 size_t i;
 
 for (i = 0; i < strlen(a); i++)
 {
 putchar(a[i]);
 }
+
+putchar('\n');
 
 return (0);
 }
