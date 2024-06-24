9648f147a43db573a3098ddc85878ba87c041193
Arnaud875 <arnaud.nickles33@gmail.com>
2024-06-24 08:20:10 +0000

    more_functions_nested_loops

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('\\');
+	}
+
+	_putchar('\n');
+}
