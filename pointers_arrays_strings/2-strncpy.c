3dc98d04412f9d2479e86810ae9874fb70a016bc
Arnaud875 <arnaud.nickles33@gmail.com>
2024-06-24 14:36:49 +0000

    pointers_arrays_strings

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,18 +11,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	i = 0;
-	while (*src)
-	{
-		if (i >= n)
-		{
-			dest[i] = '\0';
-			break;
-		}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-		dest[i++] = *src;
-		src++;
-	}
-
+	dest[i] = '\0';
 	return (dest);
 }
