3727c7e90c26713c0b3d8efc40e9e4966286110d
Arnaud875 <arnaud.nickles33@gmail.com>
2024-06-24 11:49:36 +0000

    pointers_arrays_strings

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,7 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d%s ", a[i], ((i < (n - 1)) ? "," : ""));
+		printf("%d%s ", a[i], ((i < (n - 1)) ? ", " : ""));
 	}
 
 	printf("\n");
