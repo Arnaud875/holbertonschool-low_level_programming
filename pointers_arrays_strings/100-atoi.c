7921cdea688b279f106ad201b2f77dcba06d7fc4
Arnaud875 <arnaud.nickles33@gmail.com>
2024-06-24 13:00:15 +0000

    pointers_arrays_strings

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,19 +7,27 @@
  */
 int _atoi(char *s)
 {
-	int i;
+	int i, j;
+
+	i = 0;
+	j = 1;
 
 	while (*s)
 	{
+		if (*s == '-')
+			j *= -1;
+
 		if (*s >= '0' && *s <= '9')
 		{
 			i = 0;
 			while (*s >= '0' && *s <= '9')
 			{
+				if (*s == '-')
+					j *= -1;
 				i = (i * 10) + (*s - '0');
 				s++;
 			}
-			return (i);
+			return ((j != -1) ? i : -i);
 		}
 
 		s++;
