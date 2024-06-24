95c8ecff077ab5cfe38bd20e49a3db54e4c5032c
Arnaud875 <arnaud.nickles33@gmail.com>
2024-06-24 10:41:53 +0000

    pointers_arrays_strings

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string, in reverse, followed by a new line.
+ * @s: string to be printed
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char a;
+
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+
+	for (j = 0; j <= (i / 2); j++)
+	{
+		a = *((s - 1) - j);
+		*((s - 1) - j) = *((s - i) + j);
+		*((s - i) + j) = a;
+	}
+}
