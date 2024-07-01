0229ae5a6032a64a8e70e66719850d915cb02f8a
Arnaud875 <arnaud.nickles33@gmail.com>
2024-07-01 17:51:22 +0200

    recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,5 +1,28 @@
 #include "main.h"
 
+/**
+ * prime_helper - Check if a number is prime
+ * @n: The number
+ * @i: The current divisor
+ * Return: 1 if n is a prime number, 0 no
+ */
+int prime_helper(int n, int i)
+{
+	if (n % i == 0)
+		return (0);
+	if (i * i > n)
+		return (1);
+	return (prime_helper(n, i + 1));
+}
+
+/**
+ * is_prime_number - Check if a number is prime
+ * @n: The number
+ * Return: 1 if n is a prime, 0 no
+ */
 int is_prime_number(int n)
 {
+	if (n <= 1)
+		return (0);
+	return (prime_helper(n, 2));
 }
