4e6298f1d3f7091ba3974251abdea749d9a35ea3
Arnaud875 <arnaud.nickles33@gmail.com>
2024-07-01 11:31:14 +0200

    pointers_arrays_strings

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * _strspn - Returns the number of characters without in the string
- * @s: the string
- * @accept: string that containes the characters to search for
- * Return: test
+ * _strspn - Returns the length of the initial segment of s which
+ * consists entirely of characters in accept
+ * @s: the string to be scanned
+ * @accept: string containing the characters to match
+ * Return: the number of characters in the initial segment of s
+ * which consist only of characters from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
